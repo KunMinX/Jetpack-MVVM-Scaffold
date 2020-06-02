5d83fca77f4389b3f198b79f0aaa4b26a39cf0a4
KunMinX <kunminx@gmail.com>
2020-06-02 14:39:14 +0800

    replace netState liveData observe in onViewCreated

--- app/src/main/java/com/kunminx/puremusic/ui/base/BaseFragment.java
+++ app/src/main/java/com/kunminx/puremusic/ui/base/BaseFragment.java
@@ -76,6 +76,11 @@ public abstract class BaseFragment extends Fragment {
         mSharedViewModel = ((App) mActivity.getApplicationContext()).getAppViewModelProvider(mActivity).get(SharedViewModel.class);
 
         initViewModel();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         //TODO 注意 liveData 的 lambda 回调中不可空实现，不然会出现 Cannot add the same observer with different lifecycles 的现象，
         // 详见：https://stackoverflow.com/questions/47025233/android-lifecycle-library-cannot-add-the-same-observer-with-different-lifecycle
