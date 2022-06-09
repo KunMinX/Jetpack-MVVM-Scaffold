0d32947f5d1955706d3079b5ad679187e22ef676
kunminx <kunminx@gmail.com>
2022-06-09 18:20:07 +0800

    upload class under rename category

---
+++ app/src/main/java/com/kunminx/puremusic/ui/bind/CommonBindingAdapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018-present KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kunminx.puremusic.ui.bind;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.bumptech.glide.Glide;
+import com.kunminx.architecture.utils.ClickUtils;
+
+/**
+ * Create by KunMinX at 19/9/18
+ */
+public class CommonBindingAdapter {
+
+    @BindingAdapter(value = {"imageUrl", "placeHolder"}, requireAll = false)
+    public static void imageUrl(ImageView view, String url, Drawable placeHolder) {
+        Glide.with(view.getContext()).load(url).placeholder(placeHolder).into(view);
+    }
+
+    @BindingAdapter(value = {"visible"}, requireAll = false)
+    public static void visible(View view, boolean visible) {
+        view.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter(value = {"textColor"}, requireAll = false)
+    public static void setTextColor(TextView textView, int textColorRes) {
+        textView.setTextColor(textView.getContext().getColor(textColorRes));
+    }
+
+    @BindingAdapter(value = {"selected"}, requireAll = false)
+    public static void selected(View view, boolean select) {
+        view.setSelected(select);
+    }
+
+
+    @BindingAdapter(value = {"onClickWithDebouncing"}, requireAll = false)
+    public static void onClickWithDebouncing(View view, View.OnClickListener clickListener) {
+        ClickUtils.applySingleDebouncing(view, clickListener);
+    }
+}
---
+++ app/src/main/java/com/kunminx/puremusic/ui/bind/IconBindingAdapter.java
@@ -0,0 +1,38 @@
+package com.kunminx.puremusic.ui.bind;
+
+import androidx.databinding.BindingAdapter;
+
+import com.kunminx.puremusic.ui.view.PlayPauseView;
+
+import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
+import net.steamcrafted.materialiconlib.MaterialIconView;
+
+/**
+ * Create by KunMinX at 2020/3/13
+ */
+public class IconBindingAdapter {
+
+    @BindingAdapter(value = {"isPlaying"}, requireAll = false)
+    public static void isPlaying(PlayPauseView pauseView, boolean isPlaying) {
+        if (isPlaying) {
+            pauseView.play();
+        } else {
+            pauseView.pause();
+        }
+    }
+
+    @BindingAdapter(value = {"mdIcon"}, requireAll = false)
+    public static void setIcon(MaterialIconView view, MaterialDrawableBuilder.IconValue iconValue) {
+        view.setIcon(iconValue);
+    }
+
+    @BindingAdapter(value = {"circleAlpha"}, requireAll = false)
+    public static void circleAlpha(PlayPauseView pauseView, int circleAlpha) {
+        pauseView.setCircleAlpha(circleAlpha);
+    }
+
+    @BindingAdapter(value = {"drawableColor"}, requireAll = false)
+    public static void drawableColor(PlayPauseView pauseView, int drawableColor) {
+        pauseView.setDrawableColor(drawableColor);
+    }
+}
